3eacae3eacc163f8abf2366e4b31fcb34c2d5636
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-01-30 18:46:29 -0300

    criando_evento_de_'busca'_criando_api_utilizando_'json_server'_manipulando_o_DOM

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-//BOM DIA | BOA TARDE | BOA NOITE
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
 
-// Obtém a referência do elemento com o ID "greeting"
+
+function requestApi(searchTerm) {
+  const url = `http://localhost:3000/artists?name_like=${searTerm}`;   // url da 'API'
+  fetch(url)
+  .then((response)=> response.json())
+  .then((result)=> displayResult(result))
+}
+
+function displayResults(result) {
+  resultPlaylist.classList.add('hidden');
+  const artistName = document.getElementById('artist-name');
+  const artistImage = document.getElementById('artist-img');
+
+  result.forEach(element => {
+    artistName.innerText = element.name;
+    artistImage.src = element.urlImg;
+  });
+
+  resultArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function() {
+  const searchTerm = searchInput.value.toLowerCase;
+  if(searchTerm === '') {
+    result-playlists.classList.add('hidden');
+    result-playlists.classList.remove('hidden');
+    return;
+  }
+
+  requestApi(searchTerm);
+});
+
 const greetingElement = document.getElementById("greeting");
 
 // Obtém a hora atual do sistema
